6c8ed239317da3af08d69b33e52eaadcd3225c00
Jyotsna gavtade <107462951+Jyo20@users.noreply.github.com>
2024-05-13 16:29:50 +0000

    Added useEffect Hook

--- useeffect-hook/src/App.jsx
+++ useeffect-hook/src/App.jsx
@@ -22,3 +22,84 @@ export default function App() {
    </>
   )
 }
+
+// Example two
+import React, { useEffect, useState } from "react";
+
+const App = () => {
+  let [data, setData] = useState([]);
+
+  useEffect(() => {
+    let x = window.fetch("https://fakestoreapi.com/products");
+    x.then(
+      (d) => {
+        d.json().then(
+          (v) => {
+            setData(v);
+            console.log(v);
+          },
+          (e) => {
+            console.log(e);
+          }
+        );
+      },
+      (e) => {
+        console.log(e);
+      }
+    );
+  }, []);
+
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th> Title</th>
+            <th>Description</th>
+            <th>Price</th>
+            <th>Image</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((v, i) => {
+            let { title, id, description, price, image } = v;
+            return (
+              <tr>
+                <td key={i}>{id}</td>
+                <td>{title}</td>
+                <td>{description}</td>
+                <td>{price}</td>
+                <td>
+                  <img src={image} alt="" />
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default App;
+
+// Example Three
+import React, { useEffect, useState } from "react";
+
+ const App = () => {
+   console.log(window.innerWidth); //return web page width
+   let [width, setWidth] = useState(window.innerWidth);
+  useEffect(() => {
+     window.addEventListener("resize", (e) => {
+       setWidth(window.innerWidth);
+     });
+   }, [width]);
+   return (
+     <div>
+       <h1>{width}</h1>
+     </div>
+   );
+ };
+
+ export default App;
